3ea0e4d58d6ae968a1830d950d0258c36f2fd52d
yijinc <yijinc@qq.com>
2022-12-09 17:51:52 +0800

    feat: update nav

--- packages/vue/src/components/navbar/index.vue
+++ packages/vue/src/components/navbar/index.vue
@@ -1,3 +1,74 @@
-<script>
-export { default } from './index.tsx';
+<template>
+  <view class='fish-navbar'>
+    <view
+      :class="{ 'fish-navbar__fixed': props.fixed }"
+      :style="{ backgroundColor: props.backgroundColor }"
+    >
+      <view :style="{ height: `${statusBarHeight}px` }" />
+      <view v-if="height !== 0" class='fish-navbar__header' :style="{ height, backgroundColor: props.backgroundColor }">
+        <view v-if="!props.hideBack" class='fish-navbar__header-left'>
+          <view v-if="!slots.left" class='fish-navbar__goback' @tap="handleBack">
+            <image :src="props.backIcon!" class='fish-navbar__icon' />
+          </view>
+          <block v-else>
+            <slot name="left" />
+          </block>
+              
+        </view>
+        <view class='fish-navbar__header-wrap'>
+          <text v-if="!slots.title" :style="{ color: props.color }">{{props.title}}</text>
+          <block v-else>
+            <slot name="title" />
+          </block>
+        </view>
+      </view>
+    </view>
+    <view class='fish-navbar__placeholder'>
+      <view :style="{height: `${statusBarHeight}px`}" />
+      <view :style="{ height }" />
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { withDefaults, defineProps, useSlots, computed } from 'vue';
+import Taro from '@tarojs/taro';
+import { getSizeToPx } from '../../utils';
+import iconPath from '../../../../../assets/icon-back.png';
+
+interface IProps {
+  title?: string;
+  backIcon?: string;
+  color?: string;
+  backgroundColor?: string;
+  fixed?: boolean;
+  height?: number;
+  hideBack?: boolean;
+  goback?: (e: any) => void; // eslint-disable-line no-unused-vars
+};
+
+const props = withDefaults(defineProps<IProps>(), {
+  title: '',
+  backIcon: iconPath,
+  color: '',
+  backgroundColor: '',
+  fixed: false,
+  height: 44,
+  hideBack: false,
+});
+
+const { statusBarHeight } = Taro.getSystemInfoSync();
+
+const slots = useSlots();
+
+const handleBack = (e) => {
+  if (typeof props.goback === 'function') {
+    props.goback(e);
+  } else {
+    Taro.navigateBack();
+  }
+};
+
+const height = computed(() => getSizeToPx(props.height));
+
 </script>
