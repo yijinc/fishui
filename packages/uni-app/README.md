fa7273f4df4d4567bb472b9b00c20cc916451459
yijinc <yijinc@qq.com>
2022-12-17 15:52:38 +0800

    feat: no build for uni

---
+++ packages/uni-app/README.md
@@ -0,0 +1,53 @@
+## 说明
+
+在使用小程序框架开发小程序过程中，发现很多常用的组件 社区都**找不到**合适的。
+对于相应的一些比较完善的ui组件库，他们貌似只会依赖于设计，从 Button 开始造组件。。。
+很多时候，我们可能不是需要他们的ui风格（主题），而是需要一个功能与交互相符、性能可靠的组件
+
+fishui 是一套基于 [taro](https://github.com/NervJS/taro) 和 [uni-app](https://github.com/dcloudio/uni-app) 的常用的小程序组件，致力于用最简洁、舒适可靠的实现，这里可能有你需要的而ui库没有给你提供的组件
+
+
+## 组件列表
+
+- [Navbar：导航栏](https://github.com/yijinc/fishui/tree/master/packages/uni-app/src/components/Navbar/doc.md)
+- [Tab：选项切换](https://github.com/yijinc/fishui/tree/master/packages/uni-app/src/components/Tab/doc.md)
+- [Scroll：列表滚动 下拉刷新、加载更多](https://github.com/yijinc/fishui/tree/master/packages/uni-app/src/components/Scroll/doc.md)
+- [Drag：拖拽排序 - 单屏](https://github.com/yijinc/fishui/tree/master/packages/uni-app/src/components/Drag/doc.md)
+- [DragX：拖拽排序 - 列表](https://github.com/yijinc/fishui/tree/master/packages/uni-app/src/components/DragX/doc.md)
+- VirtualSwiper：虚拟滑动
+
+
+## 安装
+
+```bash
+npm add @fishui/uni-app
+```
+
+## 使用
+
+```js
+// 引入样式（全局）
+import '@fishui/uni-app/lib/style/style.css';
+// 必须指定 .vue 原文件
+import Navbar from '@fishui/uni-app/lib/Tab/index.vue';
+```
+
+
+-  pages.json配置easycom规则(按需引入)
+
+```json
+// pages.json
+{
+	"easycom": {
+		// 下载安装的方式需要前面的"@/"，npm安装的方式无需"@/"
+		// 下载安装方式
+		"^u-(.*)": "@/uview-ui/components/u-$1/u-$1.vue"
+		// npm安装方式
+		// "^u-(.*)": "uview-ui/components/u-$1/u-$1.vue"
+	},
+	// 此为本身已有的内容
+	"pages": [
+		// ......
+	]
+}
+```
--- packages/uni-app/src/components/Navbar/index.vue
+++ packages/uni-app/src/components/Navbar/index.vue
@@ -32,8 +32,7 @@
 
 <script lang="ts" setup>
 import { withDefaults, defineProps, useSlots, computed } from 'vue';
-import { getSizeToPx } from '../../utils';
-import iconPath from '../../../../../assets/icon-back.png';
+const iconPath = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAAAXNSR0IArs4c6QAAApJJREFUaEPtmVmO1DAQhn8HhieugIQrfRceENDd0zPsF0DcBvEMM8AATyD25Sqx3ULcYRapU8hSIoVMko6TOIs0/RjZzv/VX0lXVQQm/hMT148LgKEd9OYAEd0E8JyZd4IgeKqU+uAD1gsAEe0CeAtgJxH9V2t9bRIAUsqVEOIoI97qVlrr2egBpJR7Qog3OfFnQRDciqLo16gBSsSfMvOeMeazD/H2zE6eASnlfhL5yxmh3sV3AjCbzfbjOLZp8594ACut9RdfkU/PbeVAGIZ3mfl1gfhdrfVX3+JbORCG4T1mfjWk+MYAFeKXWutvfUS+cQoR0X0Ah7nInwCwadOreGcHysQLIZZKqe99Rt7ZASJ6AOAgH/khxdd2wIpn5kMhxKVMlE+EEAul1I8hIl/bASJ6yMwHYxS/1QEp5SMAL/PigyCYR1H0c8jIb3WgRPxxHMeL9Xo9CvGlDkgpHwN4kYv8MTPPjTFeqsqmbp4rJWxts9lsjgrE3zHG/G56I1/7zgEQ0R8A2e7JRn6U4gtTiIgMgOtje12WOVjkwBLA+4I/rMHf+UUQheX0GEuG2g6kC6cCUdnQjKlsdnYg3VDRdfVe+9d+BvILx9A6NnYg3VjRvPfW/zZ2IN1YNj5JurFemvg8hPNUomyA1dcYpTWAPaBk/nk6BISzA5l0Khri9jKNy7rQGMAeUjBGt5d7hWgFUAFxxswrn0PdrR2ZS/1ORLYAfJcfqyeT6U8uZ7mube1ApnYaBKIzAAsShuGCmW0pnn5aspftB47bvoYAnQJkIGw6XUndYWZtjAld06PO+s4BEoh54kQKMZ2PfJna6UYcx88AXBVCPFFKfawTUdc1XhxwFdFm/QVAm+h1sXfyDvwDmUo+QDLH3GkAAAAASUVORK5CYII=";
 
 interface IProps {
   title?: string;
@@ -55,6 +54,14 @@ const props = withDefaults(defineProps<IProps>(), {
   height: 44,
   hideBack: false,
 });
+
+const getSizeToPx = (size: number | string): string | 0 => {
+  if (String(size).startsWith('0')) return 0;
+  if (typeof size === 'number' || /^\d+$/.test(size)) {
+    return `${size}px`;
+  }
+  return size;
+};
 
 const { statusBarHeight } = uni.getSystemInfoSync();
 
