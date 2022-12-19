b7332d7e54607c014eded2fc93f8f5e312f56937
yijinc <yijinc@qq.com>
2022-12-19 13:52:03 +0800

    feat: export virtualswiper

--- packages/taro-react/src/index.ts
+++ packages/taro-react/src/index.ts
@@ -3,3 +3,5 @@ export { default as DragX } from './components/DragX';
 export { default as Scroll } from './components/Scroll';
 export { default as Tab } from './components/Tab';
 export { default as Navbar } from './components/Navbar';
+export { default as VirtualSwiper } from './components/VirtualSwiper';
+export { default as SwiperSlide } from './components/SwiperSlide';
